32a3ef5f948a63e4d64f806e3b4dd5ccd30b0583
jiriklepl <jiriklepl@seznam.cz>
2021-03-06 04:25:54 +0100

    updated according to the assignment

---
+++ Azure Web Application/Default.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Azure_Web_Application
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            MyLabel.Text = "Hello. Now is: " + DateTime.Now.ToString();
+        }
+    }
+}
