66d8f17310f7621125e72d2e8a1a88013212a473
jiriklepl <jiriklepl@seznam.cz>
2021-04-01 02:27:30 +0200

    lab7 done

--- RedisCache/Program.cs
+++ RedisCache/Program.cs
@@ -3,32 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
+using StackExchange.Redis;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace RedisCache
 {
     class Program
     {
-        public static string Url = "http://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.txt?date={0}";
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Kurzovni listek CNB");
-            Console.WriteLine("-------------------");
-            Console.Write("Insert date in format DD.MM.YYYY: ");
-            string date = Console.ReadLine();
+            Request("Request an object", new object());
+            Request("Request a computation", "y = x  + x; x = 5");
+            Request("Request a computation", "y = x  + x; x = 5"); // again
+            Request("Request a file content", "/etc/passwd");
+            Request("Unique request " + Guid.NewGuid().ToString(), "/etc/passwd");
 
-            string url = string.Format(Url, date);
 
-            using (WebClient webClient = new WebClient())
+            lazyConnection.Value.Dispose();
+
+            Console.Write("Press any key to continue... ");
+            Console.ReadKey();
+        }
+        private static Lazy<ConnectionMultiplexer> lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+        {
+            string cacheConnection = ConfigurationManager.ConnectionStrings["RedisCacheString"].ConnectionString;
+            return ConnectionMultiplexer.Connect(cacheConnection);
+        });
+
+        public static ConnectionMultiplexer Connection
+        {
+            get
             {
-                webClient.Encoding = Encoding.UTF8;
-                string result = webClient.DownloadString(url);
-
-                Console.WriteLine();
-                Console.WriteLine(result);
-                Console.ReadKey();
+                return lazyConnection.Value;
             }
+        }
+        private static void Request(string command, object parameters)
+        {
+            IDatabase cache = Connection.GetDatabase();
+
+            var responseFactory = new Func<object, string>((object _) => "Dummy response");
+            string response;
+            bool wasCached;
+
+            if (wasCached = cache.KeyExists(command))
+            {
+                response = cache.StringGet(command);
+            }
+            else
+            {
+                response = responseFactory(parameters);
+                cache.StringSet(command, response);
+            }
+
+            long counter = cache.StringIncrement($"{command} counter", 1);
+
+            Console.WriteLine($"Command: \"{command}\" #{counter}, Response: \"{response}\" ({(wasCached ? "cached" : "not cached")})");
         }
     }
 }
